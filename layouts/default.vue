429ccdb4de482b7a5fd45d5c79c4c05b426f3003
Akkun <akkunlab@gmail.com>
2022-12-31 00:14:02 +0900

    Add Vuetify

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,5 @@
 <template>
-  <slot />
+  <v-app>
+    <slot />
+  </v-app>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  // publicRuntimeConfig: {
+  // },
+  // privateRuntimeConfig: {
+  // },
+  css: ["vuetify/lib/styles/main.sass"],
+  build: {
+    transpile: ["vuetify"],
+  },
+  vite: {
+    define: {
+      "process.env.DEBUG": false,
+    },
+  },
+  app: {
+    head: {
+      charset: 'utf-16',
+      viewport: 'width=device-width, initial-scale=1.0'
+    }
+  }
 })

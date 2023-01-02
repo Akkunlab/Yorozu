06468eb6bc8b8de153ab47e0f85dbec3a89bf75e
Akkun <akkunlab@gmail.com>
2023-01-02 23:57:02 +0900

    Update wireframe in index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,20 @@
 <template>
-  <h1>Hello index.html</h1>
+  <v-app-bar app>
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-toolbar-title>Yorozu</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn icon>
+      <v-icon>mdi-dots-vertical</v-icon>
+    </v-btn>
+  </v-app-bar>
+
+  <v-main>
+    <v-container>
+      <v-row>
+        <v-col v-for="n in 8" :key="n" cols="3">
+          <v-card height="400"></v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-main>
 </template>
